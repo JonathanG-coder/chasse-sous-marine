7a1973453d09f8a3e87b019837831eca007ec2a0
Jonathan <jonathangager75@gmail.com>
2025-07-29 14:51:36 +0200

    Chand second PORT en 3023

--- backend/server.js
+++ backend/server.js
@@ -45,7 +45,7 @@ app.use('/api/auth', authRoutes);
 app.get("/", (req, res) => {
     res.send('<h2>Bonjour du serveur</h2>');
 });
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3023;
 
 
 const server = app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
